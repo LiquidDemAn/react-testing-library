40d1a670132d1fa5880657865ef910d1f5f1ec92
NoDiggity.Dexter <dimonshinkarenko2001@gmail.com>
2022-07-23 14:31:05 +0300

    tested app component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 describe('App component', () => {
@@ -6,5 +7,38 @@ describe('App component', () => {
 		render(<App />);
 
 		expect(screen.getByRole('list')).toBeInTheDocument();
+		expect(screen.getByText('Find:')).toBeInTheDocument();
+	});
+
+	it('typing in search textbox', () => {
+		render(<App />);
+
+		expect(screen.queryByDisplayValue(/irs/)).toBeNull();
+
+		const input = screen.getByRole('textbox');
+
+		userEvent.type(input, 'irs');
+
+		expect(screen.getByText('first')).toBeInTheDocument();
+	});
+
+	it('search filter is working', () => {
+		render(<App />);
+
+		expect(screen.getByText('first')).toBeInTheDocument();
+		expect(screen.getByText('second')).toBeInTheDocument();
+
+		const input = screen.getByRole('textbox');
+
+		userEvent.type(input, 'irs');
+
+		expect(screen.queryByText('second')).toBeNull();
+		expect(screen.getByText('first')).toBeInTheDocument();
+	});
+
+	it('App snapshot', () => {
+		const view = render(<App />);
+
+		expect(view).toMatchSnapshot();
 	});
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { List } from './components/list';
 import { Search } from './components/search';
 
@@ -7,12 +7,19 @@ const data = ['first', 'second', 'third'];
 
 function App() {
 	const [search, setSearch] = useState('');
+	const [items, setItems] = useState(data);
+
+	useEffect(() => {
+		setItems(
+			data.filter((item) => item.toLowerCase().includes(search.toLowerCase()))
+		);
+	}, [search]);
 
 	return (
 		<div className='App'>
 			<div className='App-header'>
-				<Search value={search} onChange={setSearch}></Search>
-				<List list={data} />
+				<Search value={search} onChange={setSearch}>Find:</Search>
+				<List list={items} />
 			</div>
 		</div>
 	);
